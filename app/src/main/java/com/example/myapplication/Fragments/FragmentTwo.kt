dda7eb61a78fdae68032e50a97119645c4fd8356
Foks <johndoe@example.com>
2021-11-12 23:42:58 +0300

    FirstCommitSecond

--- app/src/main/java/com/example/myapplication/Fragments/FragmentTwo.kt
+++ app/src/main/java/com/example/myapplication/Fragments/FragmentTwo.kt
@@ -18,7 +18,7 @@ class FragmentTwo:Fragment() {
     ): View {
         binding = FragmentTwoBinding.inflate(inflater)
         return  binding.root
-//new
+//newFirst
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
